bd0440a948c4afb8c0fc3c214962977fe2cc9496
pyphilia <pyphilia@gmail.com>
2021-09-24 16:07:25 +0200

    refactor: register file plugins to import public endpoints

--- src/service-api.ts
+++ src/service-api.ts
@@ -2,6 +2,8 @@
 import { FastifyPluginAsync } from "fastify";
 import { Actor, IdParam } from "graasp";
 import fastifyCors from "fastify-cors";
+import graaspS3FileItem, { GraaspS3FileItemOptions } from "graasp-s3-file-item";
+import graaspFileItem from "graasp-file-item";
 // local
 import { PublicItemService } from "./db-service";
 import common, { getOne, getChildren, getItemsBy, getMember, getMembers } from "./schemas";
@@ -9,21 +11,35 @@ import { GetPublicItemTask } from "./tasks/get-public-item";
 import { GetPublicItemChildrenTask } from "./tasks/get-public-item-children";
 import { GetPublicItemWithTagTask } from "./tasks/get-public-items-by-tag-task";
 import { GetPublicMembersTask } from "./tasks/get-public-members-task";
-import { GetPublicMemberTask } from "./tasks/get-public-member-task";
+
+// todo: import type from package
+interface GraaspFileItemOptions {
+  /**
+   * Filesystem root path where the uploaded files will be saved
+   */
+  storageRootPath: string;
+}
+declare module "fastify" {
+  interface FastifyInstance {
+    s3FileItemPluginOptions?: GraaspS3FileItemOptions;
+    fileItemPluginOptions?: GraaspFileItemOptions;
+  }
+}
 
 export interface GraaspItemLoginOptions {
   /** id of the tag to look for in the item to check if an item is public */
   tagId: string;
   graaspActor: Actor;
+  enableS3FileItemPlugin?: boolean;
 }
 
 const plugin: FastifyPluginAsync<GraaspItemLoginOptions> = async (fastify, options) => {
-  const { tagId, graaspActor } = options;
+  const { tagId, graaspActor, enableS3FileItemPlugin } = options;
   const {
     items: { dbService: iS },
     taskRunner: runner,
     itemMemberships: { dbService: iMS },
-    members: { dbService: mS },
+    members: { dbService: mS, taskManager: mT },
   } = fastify;
 
   const pIS = new PublicItemService(tagId);
@@ -35,37 +51,59 @@ const plugin: FastifyPluginAsync<GraaspItemLoginOptions> = async (fastify, optio
 
   fastify.addSchema(common);
 
-  fastify.get<{ Params: IdParam; Querystring: { withMemberships?: boolean } }>(
-    "/items/:id",
-    { schema: getOne },
-    async ({ params: { id: itemId }, query: { withMemberships }, log }) => {
-      const task = new GetPublicItemTask(graaspActor, itemId, { withMemberships }, pIS, iS, iMS);
-      return runner.runSingle(task, log);
-    }
-  );
+  fastify.register(
+    () => {
+      if (enableS3FileItemPlugin) {
+        fastify.register(graaspS3FileItem, fastify.s3FileItemPluginOptions);
+      } else {
+        fastify.register(graaspFileItem, {
+          ...fastify.fileItemPluginOptions,
+          isPublic: true,
+          graaspActor,
+        });
+      }
 
-  fastify.get<{ Params: IdParam; Querystring: { ordered?: boolean } }>(
-    "/items/:id/children",
-    { schema: getChildren },
-    async ({ params: { id: itemId }, query: { ordered }, log }) => {
-      const task = new GetPublicItemChildrenTask(graaspActor, itemId, pIS, iS, ordered);
-      return runner.runSingle(task, log);
-    }
-  );
-
-  fastify.get<{ Querystring: { tagId: string; withMemberships?: boolean } }>(
-    "/items",
-    { schema: getItemsBy },
-    async ({ query: { tagId, withMemberships }, log }) => {
-      const task = new GetPublicItemWithTagTask(
-        graaspActor,
-        { tagId, withMemberships },
-        pIS,
-        iS,
-        iMS
+      fastify.get<{ Params: IdParam; Querystring: { withMemberships?: boolean } }>(
+        "/:id",
+        { schema: getOne },
+        async ({ params: { id: itemId }, query: { withMemberships }, log }) => {
+          const task = new GetPublicItemTask(
+            graaspActor,
+            itemId,
+            { withMemberships },
+            pIS,
+            iS,
+            iMS
+          );
+          return runner.runSingle(task, log);
+        }
       );
-      return runner.runSingle(task, log);
-    }
+
+      fastify.get<{ Params: IdParam; Querystring: { ordered?: boolean } }>(
+        "/:id/children",
+        { schema: getChildren },
+        async ({ params: { id: itemId }, query: { ordered }, log }) => {
+          const task = new GetPublicItemChildrenTask(graaspActor, itemId, pIS, iS, ordered);
+          return runner.runSingle(task, log);
+        }
+      );
+
+      fastify.get<{ Querystring: { tagId: string; withMemberships?: boolean } }>(
+        "/",
+        { schema: getItemsBy },
+        async ({ query: { tagId, withMemberships }, log }) => {
+          const task = new GetPublicItemWithTagTask(
+            graaspActor,
+            { tagId, withMemberships },
+            pIS,
+            iS,
+            iMS
+          );
+          return runner.runSingle(task, log);
+        }
+      );
+    },
+    { prefix: "/items" }
   );
 
   // todo: move in another repo?
@@ -74,7 +112,7 @@ const plugin: FastifyPluginAsync<GraaspItemLoginOptions> = async (fastify, optio
     "/members/:id",
     { schema: getMember },
     async ({ params: { id }, log }) => {
-      const task = new GetPublicMemberTask(graaspActor, id, pIS, iS, mS);
+      const task = mT.createGetTask(graaspActor, id);
       return runner.runSingle(task, log);
     }
   );
