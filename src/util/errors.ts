758d9a76faa2748b40623317a16d7a281cad1382
pyphilia <pyphilia@gmail.com>
2022-07-12 10:48:12 +0200

    refactor: refactor errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,42 +1,30 @@
-import { GraaspError, GraaspErrorDetails } from '@graasp/sdk';
+import { BaseGraaspError } from '@graasp/sdk';
 
-export class GraaspPublicError implements GraaspError {
-  name: string;
-  code: string;
-  message: string;
-  statusCode?: number;
-  data?: unknown;
-  origin: 'plugin' | string;
+import { PLUGIN_NAME } from './constants';
 
-  constructor({ code, statusCode, message }: GraaspErrorDetails, data?: unknown) {
-    this.name = code;
-    this.code = code;
-    this.message = message;
-    this.statusCode = statusCode;
-    this.data = data;
-    this.origin = 'plugin';
-  }
-}
-
-export class ItemNotFound extends GraaspPublicError {
+export class ItemNotFound extends BaseGraaspError {
+  origin = PLUGIN_NAME;
   constructor(data?: unknown) {
     super({ code: 'GPIERR001', statusCode: 404, message: 'Item not found' }, data);
   }
 }
 
-export class ItemNotPublic extends GraaspPublicError {
+export class ItemNotPublic extends BaseGraaspError {
+  origin = PLUGIN_NAME;
   constructor(data?: unknown) {
     super({ code: 'GPIERR002', statusCode: 403, message: 'Item is not public' }, data);
   }
 }
 
-export class CannotEditPublicItem extends GraaspPublicError {
+export class CannotEditPublicItem extends BaseGraaspError {
+  origin = PLUGIN_NAME;
   constructor(data?: unknown) {
     super({ code: 'GERR003', statusCode: 400, message: 'Cannot edit public item' }, data);
   }
 }
 
-export class CannotEditPublicMember extends GraaspPublicError {
+export class CannotEditPublicMember extends BaseGraaspError {
+  origin = PLUGIN_NAME;
   constructor(data?: unknown) {
     super({ code: 'GERR004', statusCode: 400, message: 'Cannot edit public member' }, data);
   }
