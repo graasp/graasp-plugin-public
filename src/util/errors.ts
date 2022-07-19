c4ecf1f71eac668c0589b5d514aad3a44733ef95
pyphilia <pyphilia@gmail.com>
2022-07-19 14:55:45 +0200

    refactor: update graasp sdk, errors

--- src/util/errors.ts
+++ src/util/errors.ts
@@ -1,30 +1,28 @@
-import { BaseGraaspError } from '@graasp/sdk';
+import { ErrorFactory } from '@graasp/sdk';
 
 import { PLUGIN_NAME } from './constants';
 
-export class ItemNotFound extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export const GraaspError = ErrorFactory(PLUGIN_NAME);
+
+export class ItemNotFound extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GPIERR001', statusCode: 404, message: 'Item not found' }, data);
   }
 }
 
-export class ItemNotPublic extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class ItemNotPublic extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GPIERR002', statusCode: 403, message: 'Item is not public' }, data);
   }
 }
 
-export class CannotEditPublicItem extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class CannotEditPublicItem extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GERR003', statusCode: 400, message: 'Cannot edit public item' }, data);
   }
 }
 
-export class CannotEditPublicMember extends BaseGraaspError {
-  origin = PLUGIN_NAME;
+export class CannotEditPublicMember extends GraaspError {
   constructor(data?: unknown) {
     super({ code: 'GERR004', statusCode: 400, message: 'Cannot edit public member' }, data);
   }
